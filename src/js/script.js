b00d50224f0530ac6de40b417a307cdaf6e3f783
Oleksadra Korkh <sashkacream0@gmail.com>
2023-02-17 21:25:39 +0200

    Created adaptive hero-section

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,61 @@
+
+
+/*========================================================*/
+
+/*NAVBAR*/
 const navMenu = document.querySelector('.navbar-list')
 const navIcon = document.querySelector('.navbar-icon')
+const navIconLine = document.querySelector('.navbar-icon__line')
+const navLink = document.querySelector('.navbar-list__link')
+const navListItem = document.querySelector('.navbar-list__item')
+const maxWidthPhone = window.matchMedia('(max-width:480px)')
+/*const maxWidthTablet = window.matchMedia('(max-width:992px)')*/
+const logo = document.querySelector('.header-logo')
 
-navIcon.addEventListener('click', () => {
-    navMenu.classList.toggle('active')
-    navIcon.classList.toggle('change')
-})
-function headerNavBar() {
-    const navIcon = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+window.addEventListener('load', () => {
+    /*========================================================*/
+
+    /*NAVBAR*/
+    if (maxWidthPhone.matches) {
+        navBar();
     } else {
-        x.style.display = "block";
+        widenLogo()
     }
+    /*========================================================*/
+
+    /*LOGO*/
+  /*  if(!(maxWidthPhone.matches)){
+        widenLogo();
+    }*/
+
+})
+function navBar () {
+    window.addEventListener('click', e => {
+        console.log(e.target)
+            if (e.target === navIcon || e.target === navIconLine) {
+                navMenu.classList.toggle('active');
+                navIcon.classList.toggle('change');
+                /*if (navMenu.classList.contains('active') && e.target !== navLink || e.target !== navListItem) {
+                    navMenu.classList.remove('active');
+                    navIcon.classList.remove('change');
+                }*/
+            }
+            if (e.target === navLink || e.target === navListItem) {
+                navMenu.classList.remove('active');
+                navIcon.classList.remove('change');
+            }
+    })
 }
+
+function widenLogo () {
+    logo.insertAdjacentHTML("beforeend", '<p class="header-logo__text">Forkio</p>')
+}
+
+
+
+
+
+
+
+
+
