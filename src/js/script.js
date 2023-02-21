ae0b01449a1aee4f557c05569cf681a4df5e640b
Oleksadra Korkh <sashkacream0@gmail.com>
2023-02-21 12:51:45 +0200

    Changed html structure, modified class naming, changed header.scss

--- src/js/script.js
+++ src/js/script.js
@@ -3,53 +3,44 @@
 /*========================================================*/
 
 /*NAVBAR*/
-const navMenu = document.querySelector('.navbar-list')
-const navIcon = document.querySelector('.navbar-icon')
-const navIconLine = document.querySelector('.navbar-icon__line')
-const navLink = document.querySelector('.navbar-list__link')
-const navListItem = document.querySelector('.navbar-list__item')
+const navMenu = document.querySelector('.menu-list')
+const navIcon = document.querySelector('.menu-icon')
+const navIconLine = document.querySelectorAll('.menu-icon__line')
+const navLink = document.querySelector('.menu-list__link')
+const navListItem = document.querySelector('.menu-list__item')
+const navListText = document.querySelector('.menu-list__link-text')
 const maxWidthPhone = window.matchMedia('(max-width:480px)')
 /*const maxWidthTablet = window.matchMedia('(max-width:992px)')*/
-const logo = document.querySelector('.header-logo')
+
 
 window.addEventListener('load', () => {
-    /*========================================================*/
 
     /*NAVBAR*/
     if (maxWidthPhone.matches) {
         navBar();
-    } else {
-        widenLogo()
     }
-    /*========================================================*/
-
-    /*LOGO*/
-  /*  if(!(maxWidthPhone.matches)){
-        widenLogo();
-    }*/
 
 })
 function navBar () {
     window.addEventListener('click', e => {
         console.log(e.target)
-            if (e.target === navIcon || e.target === navIconLine) {
-                navMenu.classList.toggle('active');
+            if (e.target === navIcon || e.target.parentElement === navIcon ) {
+                navMenu.classList.toggle('menu-list--active');
                 navIcon.classList.toggle('change');
                 /*if (navMenu.classList.contains('active') && e.target !== navLink || e.target !== navListItem) {
                     navMenu.classList.remove('active');
                     navIcon.classList.remove('change');
                 }*/
             }
-            if (e.target === navLink || e.target === navListItem) {
-                navMenu.classList.remove('active');
+            if ( e.target === navListText && e.target.parent !==navListItem) {
+                navMenu.classList.remove('menu-list--active');
                 navIcon.classList.remove('change');
             }
+
     })
 }
 
-function widenLogo () {
-    logo.insertAdjacentHTML("beforeend", '<p class="header-logo__text">Forkio</p>')
-}
+
 
 
 
